8f88c24e3869905db36f0953c275ba2219f30c8c
mauroSantulli <108669591+Mauro-Santulli@users.noreply.github.com>
2022-12-06 22:17:09 +0100

    Add Type aliases

---
+++ exercise-4-type-aliases-start.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+exports.__esModule = true;
+var country1 = "Nigeria";
+var country2 = "Italy";
+var country3 = "China";
+console.log(country1);
+console.log(country2);
+console.log(country3);
